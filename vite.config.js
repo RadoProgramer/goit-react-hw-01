1bf25a587198faf9f2b43cef24cbe9b5cc332609
RadoProgramer <145141434+RadoProgramer@users.noreply.github.com>
2024-08-21 12:11:07 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-
-// https://vitejs.dev/config/
+import react from "@vitejs/plugin-react";
+// <https://vitejs.dev/config/>
 export default defineConfig({
   plugins: [react()],
-  base: "/goit-react-hw-01",
   build: {
-    outDir: "./dist",
+    sourcemap: true,
   },
 });
